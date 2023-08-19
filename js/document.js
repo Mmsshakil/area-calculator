31f4c4c5a3778944bc9e6aea5cc68c22627a4513
programminghero <softhandshakil@gmail.com>
2023-08-19 22:27:34 +0600

    separate function in documents file

--- js/document.js
+++ js/document.js
@@ -1,37 +1,47 @@
 
 // take the input values
 // ------------------------
-// function inputElement(input) {
-//     const inputItem = document.getElementById(input);
-//     const inputItemString = inputItem.value;
-//     const inputValue = parseFloat(inputItemString);
-//     if(isNaN(inputValue)){
-//         alert('Please Enter Number');
-//         return 0;
-//     }
 
-//     return inputValue;
-// }
+function inputElement(input) {
+    const inputItem = document.getElementById(input);
+    const inputItemString = inputItem.value;
+    const inputValue = parseFloat(inputItemString);
+    if (isNaN(inputValue)) {
+        alert('Please Enter Number');
+        return 0;
+    }
 
-// // set the area value
-// function areaTextSet(areaText, area){
-//     const previousArea = document.getElementById(areaText);
-//     previousArea.innerText = area;
-// }
+    return inputValue;
+}
 
-// function addToCalculationEntry(areaType, area){
-//     console.log('calculation clicked');
-// }
+// set the area value
+function areaTextSet(areaText, area) {
+    const previousArea = document.getElementById(areaText);
+    previousArea.innerText = area;
+}
 
-// ---------------------------------
 
-// this was rectangle functions
-// first call the button 
-// function buttonRectangle() {
+// calculation entry function right side part
+// vvvvv....vvvvvv.....ppppp
+function addToCalculationEntry(areaType, area) {
+    // console.log(areaType + ' ' +area);
+    const calculationEntry = document.getElementById('calculation-entry');
 
-//     const rectangleWidth = inputElement('rectangleW');
-//     const rectangleLength = inputElement('rectanglel');
-//     const totalArea = rectangleWidth * rectangleLength;
-//     areaTextSet('rectangleAreaText', totalArea);
 
-// }
+    // for serial 1,2,3,4,
+    const count = calculationEntry.childElementCount;
+
+    // creat an element you want to add
+    const p = document.createElement('p');
+
+    // for gap of two line
+    p.classList.add('my-4');
+
+    // p.innerHTML = areaType + ' ' + area;
+    p.innerHTML = `${count + 1} ${areaType} ${area} cm<sup>2</sup>`;
+
+
+
+    calculationEntry.appendChild(p);
+}
+
